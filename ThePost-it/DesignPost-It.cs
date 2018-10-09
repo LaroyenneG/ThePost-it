2ad27314d198fe1351098336217eb2700091350d
laroyenneg <laroyenneg@gmail.com>
2018-10-09 17:18:48 +0200

    mouse move

--- ThePost-it/DesignPost-It.cs
+++ ThePost-it/DesignPost-It.cs
@@ -10,7 +10,10 @@ namespace ThePost_it
 {
     class DesignPost_It : Panel
     {
-        TextBox tb;
+
+        private Point mousePosition;
+
+        private TextBox tb;
 
         private const int MARGIN_SIZE = 15;
         private static Color DEFAULT_COLOR = Color.Yellow;
@@ -23,8 +26,13 @@ namespace ThePost_it
 
             InitializeComponent();
 
+            mousePosition = new Point(0, 0);
+
             this.model = model;
             this.tb.TextChanged += new EventHandler(ActionTextChanged);
+            this.MouseDown += new MouseEventHandler(ActionMouseDown);
+            this.MouseUp += new MouseEventHandler(ActionMouseUp);
+            this.MouseMove += new MouseEventHandler(ActionMouseMove);
             this.Controls.Add(tb);
 
             Display();
@@ -40,6 +48,7 @@ namespace ThePost_it
             this.tb = new TextBox();
             this.tb.Size = new Size(POST_SIZE.Width - MARGIN_SIZE * 2, POST_SIZE.Height - MARGIN_SIZE * 2);
             this.tb.BorderStyle = BorderStyle.None;
+            this.BorderStyle = BorderStyle.None;
             SetColor(DEFAULT_COLOR);
             this.tb.Multiline = true;
             this.tb.Location = new Point(MARGIN_SIZE, MARGIN_SIZE);
@@ -64,10 +73,40 @@ namespace ThePost_it
         }
 
 
+        public void ActionMouseMove(Object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Location = new Point(this.Location.X + e.X, this.Location.Y + e.Y);
+            }
+
+
+            mousePosition = e.Location;
+        }
+
+        public void ActionMouseUp(Object sender, MouseEventArgs e)
+        {
+            Deseleted();
+        }
+
+        public void ActionMouseDown(Object sender, MouseEventArgs e)
+        {
+            Selected();
+        }
+
         public void ActionTextChanged(object sender, EventArgs e)
         {
             this.model.SetText(this.tb.Text);
-            Console.WriteLine(this.model);
+        }
+
+        public void Selected()
+        {
+            this.BackColor = Color.Gold;
+        }
+
+        public void Deseleted()
+        {
+            this.BackColor = DEFAULT_COLOR;
         }
 
         public void LockFocus()
