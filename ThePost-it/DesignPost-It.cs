f624c189ec66922acbe71fba42eef696d3ea4b02
laroyenneg <laroyenneg@gmail.com>
2018-10-11 10:35:37 +0200

    bad solution

--- ThePost-it/DesignPost-It.cs
+++ ThePost-it/DesignPost-It.cs
@@ -13,9 +13,12 @@ namespace ThePost_it
 
         private Point mousePosition;
 
+        private bool isSelected;
+
         private TextBox tb;
 
         private const int MARGIN_SIZE = 15;
+        private static Color DEFAULT_COLOR = Color.Yellow;
         private static Size POST_SIZE = new Size(200, 200);
 
         private Post_it model;
@@ -34,7 +37,7 @@ namespace ThePost_it
             this.MouseMove += new MouseEventHandler(ActionMouseMove);
             this.Controls.Add(tb);
 
-            SetColor(this.model.GetColor());
+            isSelected = false;
 
             Display();
         }
@@ -50,6 +53,7 @@ namespace ThePost_it
             this.tb.Size = new Size(POST_SIZE.Width - MARGIN_SIZE * 2, POST_SIZE.Height - MARGIN_SIZE * 2);
             this.tb.BorderStyle = BorderStyle.None;
             this.BorderStyle = BorderStyle.None;
+            SetColor(DEFAULT_COLOR);
             this.tb.Multiline = true;
             this.tb.Location = new Point(MARGIN_SIZE, MARGIN_SIZE);
             LockFocus();
@@ -65,6 +69,11 @@ namespace ThePost_it
         {
             this.BackColor = color;
             this.tb.BackColor = color;
+        }
+
+        public Color GetColor()
+        {
+            return this.tb.BackColor;
         }
 
 
@@ -98,11 +107,13 @@ namespace ThePost_it
 
         public void Selected()
         {
+            this.isSelected = true;
             this.BackColor = Color.Gold;
         }
 
         public void Deseleted()
         {
+            this.isSelected = false;
             this.BackColor = DEFAULT_COLOR;
         }
 
@@ -114,6 +125,11 @@ namespace ThePost_it
         public void UnLockFocus()
         {
             this.Enabled = true;
+        }
+
+        public bool IsSelect()
+        {
+            return isSelected;
         }
     }
 }
--- ThePost-it/Post-it.cs
+++ ThePost-it/Post-it.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,30 +9,24 @@ namespace ThePost_it
     class Post_it
     {
 
-        private static Color DEFAULT_COLOR = Color.Yellow;
-
-        private Color color;
-        private bool selected;
         private int x;
         private int y;
 
         private string text;
 
-        public Post_it(int x, int y, string text, Color color)
+        public Post_it(int x, int y, string text)
         {
             this.x = x;
             this.y = y;
             this.text = text;
-            this.color = color;
-            selected = false;
         }
 
-        public Post_it() : this(0, 0, "", DEFAULT_COLOR)
+        public Post_it() : this(0, 0, "")
         {
-        
+         
         }
 
-        public Post_it(int x, int y) : this(x, y, "", DEFAULT_COLOR)
+        public Post_it(int x, int y) : this(x, y, "")
         {
 
         }
@@ -70,33 +63,13 @@ namespace ThePost_it
 
         public override string ToString()
         {
+
             string s = "Post-it :\n";
             s += "X=" + this.x + "\n";
             s += "Y=" + this.y + "\n";
             s += "Text=" + this.text + "\n";
-            s += "Is selected" + this.selected + "\n";
 
             return s;
-        }
-
-        public bool IsSelected()
-        {
-            return selected;
-        }
-
-        public void setSelected(bool b)
-        {
-            this.selected = b;
-        }
-
-        public void SetColor(Color color)
-        {
-            this.color = color;
-        }
-
-        public Color GetColor()
-        {
-            return this.color;
         }
     }
 }
